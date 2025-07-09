fad0eac8cfd2ce7c2372ee0d0d2810f2110cb7a5
LENOVO <v.chandra.sekhar.reddy108@gmail.com>
2025-07-09 07:14:52 +0530

    Add project files.

---
+++ MoviezzzzApp/config/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MoviezzzzApp.config
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
